144651868a687f594e7ce5f4ce18e0eec951c8e6
lordimpi <snt-26@hotmail.com>
2020-07-22 20:42:56 -0500

    Add Partials Views

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -11,18 +11,50 @@ namespace NetCore.Controllers
 {
     public class AsignaturaController : Controller
     {
-        
         public IActionResult Index()
         {
             var asignatura = new AsignaturaViewModel
             {
-                UniqueId = Guid.NewGuid().ToString(),
-                Nombre = "Programación"
+                Nombre = "Programación",
+                UniqueId = Guid.NewGuid().ToString()
+            };
+            return View(asignatura);
+        }
+
+        public IActionResult MultiAsignatura()
+        {
+            var listaAsignaturas = new List<AsignaturaViewModel>
+            {
+                new AsignaturaViewModel
+                {
+                    Nombre = "Matemáticas",
+                    UniqueId = Guid.NewGuid().ToString()
+                },
+                new AsignaturaViewModel
+                {
+                    Nombre="Educación Física",
+                    UniqueId = Guid.NewGuid().ToString()
+                },
+                new AsignaturaViewModel
+                {
+                    Nombre="Castellano",
+                    UniqueId = Guid.NewGuid().ToString()
+                },
+                new AsignaturaViewModel
+                {
+                    Nombre="Ciencias Naturales",
+                    UniqueId = Guid.NewGuid().ToString()
+                },
+                new AsignaturaViewModel
+                {
+                    Nombre="Programación",
+                    UniqueId = Guid.NewGuid().ToString()
+                }
             };
 
             ViewBag.Fecha = DateTime.UtcNow;
 
-            return View(asignatura);
+            return View("MultiAsignatura", listaAsignaturas);
         }
     }
 }
